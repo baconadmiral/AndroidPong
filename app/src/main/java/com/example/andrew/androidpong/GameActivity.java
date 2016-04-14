f84b0fdbdd15b99aa4ae2edafcabd33f12ad9128
Andrew Williams <andrewwilliams1087@gmail.com>
2016-04-14 16:19:16 -0600

    Original Restored Properly this Time

--- app/src/main/java/com/example/andrew/androidpong/GameActivity.java
+++ app/src/main/java/com/example/andrew/androidpong/GameActivity.java
@@ -9,18 +9,29 @@ public class GameActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu;
-
+        getMenuInflater().inflate(R.menu.menu_game, menu);
+        return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 
--- app/src/main/java/com/example/andrew/androidpong/GameView.java
+++ app/src/main/java/com/example/andrew/androidpong/GameView.java
@@ -1,6 +1,7 @@
 package com.example.andrew.androidpong;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Handler;
 import android.os.Message;
 import android.util.AttributeSet;
@@ -19,7 +20,22 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback
     private boolean gameRunning = false;
 
     public GameView(Context context, AttributeSet attrs) {
+        super(context, attrs);
 
+        //So we can listen for events...
+        this.context = context;
+        SurfaceHolder holder = getHolder();
+        holder.addCallback(this);
+        setFocusable(true);
+        handler = new Handler(){
+            @Override
+        public void handleMessage(Message msg){
+                sendVictoryMail();
+            }
+        };
+
+        //and instantiate the thread
+        thread = new GameThread(holder, context, handler);
     }
 
     //Implemented as part of the SurfaceHolder.Callback interface
@@ -33,13 +49,13 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback
     //Implemented as part of the SurfaceHolder.Callback interface
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
-
+            thread.start();
     }
 
     //Implemented as part of the SurfaceHolder.Callback interface
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
-
+            thread.stopGame();
     }
 
     /*
@@ -48,10 +64,15 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback
 * calls getGameState().motionDetected() if there’s a touch on the screen.
 */
     public boolean onTouchEvent(MotionEvent event) {
+        return thread.getGameState().motionDetected(event);
     }
 
     private void sendVictoryMail()
     {
-
+        //Implicit intent call
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.setType("text/plain");
+        context.startActivity(sendIntent);
     }
 }
